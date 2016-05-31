1e89c15c7e3523eeaa5177b0c4301ffb18351e1a
jonsambro <jonsambro@gmail.com>
2016-05-31 17:39:18 -0400

    Updated get_markers and worked on the database

--- mainSite/ajax.py
+++ mainSite/ajax.py
@@ -8,9 +8,16 @@ def cities(request):
 
 def markers(request):
     print(request)
-    return JsonResponse(get_markers(), safe=False);
+    return JsonResponse(get_markers(request.GET.get('OCC_ID','00-0000')), safe=False);
 
 
-def get_markers():
-    cities = list(models.City.objects.all().exclude(lat__isnull=True).values())
-    return cities;
+def get_markers(id):
+    salaries = models.Salary.objects.filter(occ_code=id, city__lat__isnull=False)
+    salaries = list(salaries.values('city__name','city__lat','city__lng','city__costOfLiving','a_median'))
+    for x in salaries:
+        print(type(x))
+        x['name'] = x.pop('city__name')
+        x['lat'] = x.pop('city__lat')
+        x['lng'] = x.pop('city__lng')
+        x['costOfLiving'] = x.pop('city__costOfLiving')
+    return salaries;
